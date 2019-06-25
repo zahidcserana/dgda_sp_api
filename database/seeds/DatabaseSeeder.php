15727e21a9191a29e838f2e1fae279fa654864d2
zahidcserana <zahid1004048@gmail.com>
2019-06-25 10:04:53 +0600

    medicine company factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             MedicinesTableSeeder::class,
+            MedicineCompaniesTableSeeder::class,
         ]);
     }
 }
