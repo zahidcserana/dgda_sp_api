2dcfd5e2499b31776c58e973e8f4f53de36f8ead
zahidcserana <zahid1004048@gmail.com>
2019-07-30 14:41:44 +0600

    test

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -138,7 +138,7 @@ class Order extends Model
             ->where('pharmacy_branch_id', $user->pharmacy_branch_id)
             ->where('company_id', $data['company_id'])
             ->first();
-
+            return ['success' => false, 'error' => 'Something went wrong!'];
         if ($order) {
             $orderId = $order->id;
         } else {
